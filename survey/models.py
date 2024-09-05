92c0bb899e264ff736d40cbf470a02f2966cad1a
takaiakio <akio.takai@alpsalpine.com>
2024-09-05 13:01:00 +0900

    nadake

--- survey/models.py
+++ survey/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class NearMiss(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    encounters = models.PositiveIntegerField()
-    prevention = models.TextField()
+class Survey(models.Model):
+    title = models.CharField(max_length=100)
+    scene = models.TextField()
+    times = models.IntegerField()
+    avoid_reason = models.TextField()
 
     def __str__(self):
         return self.title
--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,4 @@
+from .models import Survey  # models.pyからSurveyモデルをインポート
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import NearMiss
 from .forms import NearMissForm
@@ -34,15 +35,21 @@ def edit_near_miss(request, pk):
     return render(request, 'survey/form.html', {'form': form})
 
 def export_csv(request):
+    # HttpResponseでCSVのレスポンスを作成
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="near_miss_data.csv"'
+    response['Content-Disposition'] = 'attachment; filename="survey_data.csv"'
 
-    writer = csv.writer(response, encoding='utf-8')
-    writer.writerow(['Title', 'Description', 'Encounters', 'Prevention'])
-
-    for near_miss in NearMiss.objects.all():
-        writer.writerow([near_miss.title, near_miss.description, near_miss.encounters, near_miss.prevention])
-
+    # CSVの書き込み処理
+    writer = csv.writer(response)
+    
+    # カラムヘッダーを追加
+    writer.writerow(['題名', '遭遇場面', '遭遇回数', 'ミス回避要因'])
+    
+    # データベースからデータを取得してCSVに書き込む
+    survey_data = Survey.objects.all()  # Surveyはアンケートデータのモデル名
+    for data in survey_data:
+        writer.writerow([data.title, data.scene, data.times, data.avoid_reason])
+    
     return response
 
 
